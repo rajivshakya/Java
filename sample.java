845285313f0066589ddad3f3997fb5699599ac68
rajivshakya <89643654+rajivshakya@users.noreply.github.com>
2021-10-31 21:45:56 +0530

    Update sample.java

--- sample.java
+++ sample.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello Java First Update");  
+     System.out.println("Hello Java First Update Second Update");  
     }  
 }  
